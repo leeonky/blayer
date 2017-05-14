7d06d84d5cee1c2db7b8e7572d7ec1f9898e4452
leeonky <leeonky@gmail.com>
2017-05-14 15:36:45 +0000

    get repo url from git remote -v

--- clean_test.sh
+++ clean_test.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+git_repo=$(git remote -v | grep fetch | awk '{sub("git@github.com:", "https://github.com/", $2); print $2}')
+
 sudo docker run --rm -ti daocloud.io/leeonky/ctest runtest https://github.com/leeonky/blayer.git
