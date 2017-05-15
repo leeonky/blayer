0eb6f8b094fd36992a2509692e045ba673f29a1c
leeonky <leeonky@gmail.com>
2017-05-15 09:13:45 +0000

    mock test for snubber

--- snubber_test/main.c
+++ snubber_test/main.c
@@ -3,7 +3,17 @@
 #include "test.h"
 #include "snubber/snubber.h"
 
-static void test_pass_through_and_exit() {
+FILE *input_stdin, *input_stdout, *input_stderr;
+
+int pinf_main(FILE *stdin, FILE *stdout, FILE *stderr) {
+	input_stdin = stdin;
+	input_stdout = stdout;
+	input_stderr = stderr;
+	return 100;
+}
+
+
+static void test_invoke_pinf_main() {
 	char input_buffer[1024] = "Hello world!\nEXIT\ndump";
 	char output_buffer[1024] = {};
 	char error_buffer[1024] = {};
@@ -11,11 +21,13 @@ static void test_pass_through_and_exit() {
 	FILE *output_stream = fmemopen(output_buffer, sizeof(output_buffer), "w");
 	FILE *error_stream = fmemopen(error_buffer, sizeof(error_buffer), "w");
 
-	CU_ASSERT_EQUAL(snubber_main(0, NULL, input_stream, output_stream, error_stream), 0);
+	CU_ASSERT_EQUAL(snubber_main(0, NULL, input_stream, output_stream, error_stream), 100);
+	CU_ASSERT_EQUAL(input_stdin, input_stream);
+	CU_ASSERT_EQUAL(input_stdout, output_stream);
+	CU_ASSERT_EQUAL(input_stderr, error_stream);
 	fclose(input_stream);
 	fclose(output_stream);
 	fclose(error_stream);
-	CU_ASSERT_STRING_EQUAL(output_buffer, "Hello world!\n")
 }
 
 int main() {
@@ -23,7 +35,7 @@ int main() {
 	init_test();
 
 	suite = create_suite("snubber test", NULL, NULL);
-	add_case(suite, test_pass_through_and_exit);
+	add_case(suite, test_invoke_pinf_main);
 
 	return run_test();
 }
