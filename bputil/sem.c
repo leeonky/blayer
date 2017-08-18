465befae7c7816ef966e80738962c5f68e92f0e1
leeonky <leeonky@gmail.com>
2017-08-18 04:07:27 +0000

    inplementation sem_xx helper function

--- bputil/sem.c
+++ bputil/sem.c
@@ -1,15 +1,23 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
+
+static const char *sem_name() {
+	static __thread char buffer[128];
+	sprintf(buffer, "/%d", getppid());
+	return buffer;
+}
 
 sem_t *sem_new_with_ppid(int value) {
-	/*return sem_open(name, flag, mode, value);*/
+	return sem_open(sem_name(), O_CREAT|O_EXCL, 0644, value);
 }
 
 sem_t *sem_load_with_ppid() {
-	/*return sem_open(name, flag);*/
+	return sem_open(sem_name(), 0);
 }
 
 int sem_unlink_with_ppid() {
-	return 0;
+	return sem_unlink(sem_name());
 }
