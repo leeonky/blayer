c4dc6598c7eba310debb36bb91b7853f7bf15f67
leeonky <leeonky@gmail.com>
2017-08-21 05:54:54 +0000

    try to create renderer in soft mode

--- wrpsdl_test/sdl_window_test.c
+++ wrpsdl_test/sdl_window_test.c
@@ -80,7 +80,7 @@ SUITE_CASE("open window with args and get window and renderer and texture") {
 	CUE_EXPECT_CALLED_ONCE(SDL_CreateRenderer);
 	CUE_EXPECT_CALLED_WITH_PTR(SDL_CreateRenderer, 1, window);
 	CUE_EXPECT_CALLED_WITH_INT(SDL_CreateRenderer, 2, -1);
-	CUE_EXPECT_CALLED_WITH_INT(SDL_CreateRenderer, 3, SDL_RENDERER_PRESENTVSYNC);
+	CUE_EXPECT_CALLED_WITH_INT(SDL_CreateRenderer, 3, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 	CUE_EXPECT_CALLED_ONCE(SDL_CreateTexture);
 	CUE_EXPECT_CALLED_WITH_PTR(SDL_CreateTexture, 1, renderer);
@@ -117,8 +117,10 @@ SUITE_CASE("failed to init video") {
 
 	CUE_EXPECT_NEVER_CALLED(SDL_CreateWindow);
 	CUE_EXPECT_NEVER_CALLED(SDL_CreateRenderer);
-	CUE_EXPECT_NEVER_CALLED(SDL_DestroyWindow);
+	CUE_EXPECT_NEVER_CALLED(SDL_CreateTexture);
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyTexture);
 	CUE_EXPECT_NEVER_CALLED(SDL_DestroyRenderer);
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyWindow);
 	CUE_EXPECT_NEVER_CALLED(SDL_QuitSubSystem);
 }
 
@@ -134,9 +136,10 @@ SUITE_CASE("failed to create window") {
 	CUE_ASSERT_STDERR_EQ("Error[libwrpsdl]: sdl error\n");
 
 	CUE_EXPECT_NEVER_CALLED(SDL_CreateRenderer);
-	CUE_EXPECT_NEVER_CALLED(SDL_DestroyWindow);
+	CUE_EXPECT_NEVER_CALLED(SDL_CreateTexture);
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyTexture);
 	CUE_EXPECT_NEVER_CALLED(SDL_DestroyRenderer);
-
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyWindow);
 	CUE_EXPECT_CALLED_ONCE(SDL_QuitSubSystem);
 }
 
@@ -151,8 +154,44 @@ SUITE_CASE("failed to create renderer") {
 
 	CUE_ASSERT_STDERR_EQ("Error[libwrpsdl]: sdl error\n");
 
+	CUE_EXPECT_NEVER_CALLED(SDL_CreateTexture);
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyTexture);
 	CUE_EXPECT_NEVER_CALLED(SDL_DestroyRenderer);
+	CUE_EXPECT_CALLED_ONCE(SDL_DestroyWindow);
+	CUE_EXPECT_CALLED_ONCE(SDL_QuitSubSystem);
+}
 
+static SDL_Texture *stub_SDL_CreateTexture_failed(SDL_Renderer *rd, Uint32 format, int mode, int w, int h) {
+	return NULL;
+}
+
+SUITE_CASE("failed to create texture") {
+	init_mock_function(SDL_CreateTexture, stub_SDL_CreateTexture_failed);
+
+	CUE_ASSERT_SUBJECT_FAILED_WITH(-1);
+
+	CUE_ASSERT_STDERR_EQ("Error[libwrpsdl]: sdl error\n");
+
+	CUE_EXPECT_NEVER_CALLED(SDL_DestroyTexture);
+	CUE_EXPECT_CALLED_ONCE(SDL_DestroyRenderer);
+	CUE_EXPECT_CALLED_ONCE(SDL_DestroyWindow);
+	CUE_EXPECT_CALLED_ONCE(SDL_QuitSubSystem);
+}
+
+static SDL_Renderer *stub_SDL_CreateRenderer_allow_soft_mode(SDL_Window *w, int index, Uint32 flag) {
+	if (SDL_RENDERER_SOFTWARE == flag)
+		return renderer;
+	return NULL;
+}
+
+SUITE_CASE("try to create renderer in soft mode") {
+	init_mock_function(SDL_CreateRenderer, stub_SDL_CreateRenderer_allow_soft_mode);
+
+	CUE_ASSERT_SUBJECT_FAILED_WITH(10);
+
+	CUE_EXPECT_CALLED_ONCE(SDL_CreateTexture);
+	CUE_EXPECT_CALLED_ONCE(SDL_DestroyTexture);
+	CUE_EXPECT_CALLED_ONCE(SDL_DestroyRenderer);
 	CUE_EXPECT_CALLED_ONCE(SDL_DestroyWindow);
 	CUE_EXPECT_CALLED_ONCE(SDL_QuitSubSystem);
 }
